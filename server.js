226c2bf3cf1df6b532f927db0143a156e83b7318
BHARGAV-S54 <sanaboyinabhargav@gmail.com>
2025-10-04 21:23:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
 
+const authRoutes = require('./routes/auth');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+      console.log('Server running on port', process.env.PORT || 3000);
+    });
+  })
+  .catch(err => console.error(err));
