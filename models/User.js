b6d23320e277f9bbdfd5ebe6e70d859ab1a5d5b6
BHARGAV-S54 <sanaboyinabhargav@gmail.com>
2025-10-04 21:22:05 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,13 @@
 
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  fullName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
+  passwordHash: { type: String, required: true },
+  isVerified: { type: Boolean, default: false },
+  verificationToken: String,
+});
+
+module.exports = mongoose.model('User', userSchema);
